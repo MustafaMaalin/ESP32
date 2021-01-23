b389960d9a9733c8ccfc3a4ad352514730771215
Mustafa <mustimustafa5@gmail.com>
2021-01-23 11:02:32 +0100

    xml http request temp ajax

--- esp32_jan21a/esp32_jan21a.ino
+++ esp32_jan21a/esp32_jan21a.ino
@@ -32,6 +32,7 @@ String readTemp() {
   }
 }
 
+// string that reads the humidity of the weather.
 String readHum() {
   float h = dht.readHumidity();
   if (isnan(h)) {
@@ -42,3 +43,62 @@ String readHum() {
     return String(h);
   }
 }
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <style>
+    html {
+     font-family: Verdana, Geneva, Tahoma, sans-serif;
+     display: inline-block;
+     text-align: center;
+     background-color: lightblue;
+    }
+    h2 { font-size: 60px; }
+    p { font-size: 20px; }
+    .units { font-size: 1.2rem; }
+    .sensorinfo{
+      font-size: 20px;
+      text-align: center;
+    }
+    .border{
+        border: ridge; 
+        background: gray;
+    }
+  </style>
+</head>
+<body>
+  <h2>Temp/Hum check</h2>
+  <div class="border">
+  <p>
+    <i class="fas fa-thermometer-half" style="color:red"></i> 
+    <span class="sensorinfo">Temperature</span> 
+    <span id="temperature">%TEMPERATURE%</span>
+    <sup class="units">&deg;C</sup>
+  </p>
+  <p>
+    <i class="fas fa-tint" style="color:#00add6;"></i>
+     
+    <span class="sensorinfo">Humidity/Vochtigheid</span>
+    <span id="humidity">%HUMIDITY%</span>
+    <sup class="units">&percnt;</sup>
+  </p>
+</div>
+</body>
+//ajax functionalities for temp and hum
+setInterval(function(){
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function(){
+    if(this.readystate == 4 && this.status == 200){
+      document.getElementById("temperature").innerHTML = this.responseText;
+    }
+    };
+    xhttp.open("GET", "/temperature", true);
+    xhttp.send();
+  }, 10000);
+
+  
+</html>
+)
