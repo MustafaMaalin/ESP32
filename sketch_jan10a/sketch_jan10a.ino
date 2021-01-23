051b1834f801ba00255fd02630d2319a1a5bbd36
Mustafa <mustimustafa5@gmail.com>
2021-01-23 14:23:18 +0100

    ultra sone sensor extra project met buzzer

--- sketch_jan10a/sketch_jan10a.ino
+++ sketch_jan10a/sketch_jan10a.ino
@@ -1,26 +1,36 @@
-/* How to use the HC-SR04 Ultrasonic Sensor with Arduino 
-   Dev: Michalis Vasilakis // Date: 23/7/2015 // www.ardumotive.com */
 
-//Libraries
-#include "Ultrasonic.h"
+// Define pins for ultrasonic and buzzer
+int const trigger = 10;
+int const echo = 9;
+int const buzz = 2;
 
-//Define pins ultrasonic(trig,echo)
-Ultrasonic ultrasonic(A0,A1);
-
-//Variables
-int distance;
-
-void setup() {
-  Serial.begin(9600);
+void setup()
+{
+  pinMode(trigger, OUTPUT); // trig pin will have pulses output
+  pinMode(echo, INPUT); // echo pin should be input to get pulse width
+  pinMode(buzz, OUTPUT); // buzz pin is output to control buzzering
 }
 
 void loop()
 {
-  distance = ultrasonic.Ranging(CM); //Use 'CM' for centimeters or 'INC' for inches
-  //Print distance...
-  Serial.print("Object found at: ");
-  Serial.print(distance);
-  Serial.println("cm");
-  //every 1sec. 
-  delay(1000);
+  // Duration will be the input pulse width and distance will be the distance to the obstacle in centimeters
+  int duration, distance;
+  // Output pulse with 1ms width on trigger
+  digitalWrite(trigger, HIGH); 
+  delay(1);
+  digitalWrite(trigger, LOW);
+  // Measure the pulse input in echo pin
+  duration = pulseIn(echo, HIGH);
+  // Distance is half the duration devided by 29.1 (from datasheet)
+  distance = (duration/2) / 29.1;
+  // if distance less than 0.15 meter and more than 0 (0 or less means over range) 
+    if (distance <= 15 && distance >= 0) {
+      // Buzz
+      digitalWrite(buzz, HIGH);
+    } else {
+      // Don't buzz
+      digitalWrite(buzz, LOW);
+    }
+    // Waiting 60 ms 
+    delay(60);
 }
