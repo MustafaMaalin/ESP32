92e07dbf2b4d0b5106b70b8de7fcf76d9ba33392
Mustafa <mustimustafa5@gmail.com>
2021-01-23 12:48:22 +0100

    the rest of functions added. there is a boot error which prevents my esp wife connector to connect with the dht11

--- esp32_jan21a/esp32_jan21a.ino
+++ esp32_jan21a/esp32_jan21a.ino
@@ -1,15 +1,11 @@
-#include <AsyncTCP.h>
-#include <Adafruit_Sensor.h>
 #include "WiFi.h"
 #include "ESPAsyncWebServer.h"
-#include <ESPAsyncTCP.h>
+#include <Adafruit_Sensor.h>
 #include <DHT.h>
-#include <DHTesp.h>
-
 
 // Replace with your network credentials
-const char* ssid = "REPLACE_WITH_YOUR_SSID";
-const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+const char* ssid = "Familie Maalin";
+const char* password = "tijdelijk";
 
 #define DHTPIN 27     // Digital pin connected to the DHT sensor
 #define DHTTYPE    DHT11     // DHT 11
@@ -44,6 +40,7 @@ String readHum() {
   }
 }
 
+//progmem will store a webpage in a arduino program.
 const char index_html[] PROGMEM = R"rawliteral(
 <!DOCTYPE HTML><html>
 <head>
@@ -87,7 +84,7 @@ const char index_html[] PROGMEM = R"rawliteral(
   </p>
 </div>
 </body>
-//ajax functionalities for temp and hum
+//ajax functionalities for temp and hum. these js functions will update the temp and humidty every 10 seconds. 
 setInterval(function(){
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function(){
@@ -98,7 +95,69 @@ setInterval(function(){
     xhttp.open("GET", "/temperature", true);
     xhttp.send();
   }, 10000);
-
   
-</html>
-)
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("humidity").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/humidity", true);
+  xhttp.send();
+}, 10000 ) ;
+
+</script>
+  
+</html>)rawliteral";
+
+// to replace the the placeholder i'll use the function down below
+
+String processor(const String& var) {
+  // if the webpage is requested  this statement checks if there is any placeholder that is named %TEMPERATURE%
+  if (var == "TEMPERATURE") {
+    return readTemp();
+  }
+  //same goes for %HUMIDITY%
+  else if (var == "HUMIDITY") {
+    return readHum();
+  }
+  return String();
+}
+
+void setup() {
+  // serial port for debugging purposes
+  Serial.begin(115200);
+
+  dht.begin();
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to wifi...");
+  }
+}
+
+// Print Local IP Address
+Serial.println(WiFi.localIP());
+
+// Route for root / web page
+server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+  request->send_P(200, "text/html", index_html, processor);
+});
+server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request) {
+  request->send_P(200, "text/plain", readTemp().c_str());
+});
+server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request) {
+  request->send_P(200, "text/plain", readHum().c_str());
+});
+}
+
+// Start server
+server.begin();
+}
+
+void loop() {
+
+}
